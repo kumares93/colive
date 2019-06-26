f6061c418510a284afbbcd5662668e5adfd53e57
kumares93 <kumares93@gmail.com>
2019-06-26 23:29:12 +0530

    First commit

---
+++ src/app/components/leftnav/leftnav.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+    selector: 'leftnav',
+    templateUrl: './leftnav.component.html',
+    styleUrls: [ './leftnav.component.css' ]
+})
+export class LeftNav {
+
+
+}
